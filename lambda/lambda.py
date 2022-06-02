0734b761d0609239fed0db65bf37c878edeb4e8e
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-06-02 16:55:31 +0530

    fix: object name retrival

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -3,6 +3,6 @@ import urllib.parse
 
 def handler(event, context):
     source_bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['bucket'])
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
     print(f"uploaded file name is {key}")
     return {'status': 'success', 'key': key}
